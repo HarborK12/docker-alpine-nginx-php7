10c51b9657a683c5dd83a117e6b3cbe3aae08fed
Michael Martin <martinmike2@gmail.com>
2017-10-06 13:05:36 -0400

    reverting my mistakes

--- rootfs/scripts/envars.sh
+++ rootfs/scripts/envars.sh
@@ -1,8 +1,4 @@
-#!/usr/bin/with-contenv bash
-
-environment="$(curl -s http://169.254.169.254/latest/user-data | grep 'export ENVIRONMENT' | cut -f2 -d=)"
-export ENVIRONMENT=${environment}
-vars=$(curl "${ENVIRONMENT_URL}/get?key=${ENVIRONMENT}_ENV_VARS&&version=${VERSION}")
+#!/bin/bash
 
 #----------------------------------------------------------
 # Add the container environment variables to bash sessions
@@ -10,45 +6,33 @@ vars=$(curl "${ENVIRONMENT_URL}/get?key=${ENVIRONMENT}_ENV_VARS&&version=${VERSI
 
 pattern='if [ -f /opt/envars.sh ]; then source /opt/envars.sh; fi'
 source=/opt/envars.sh
-app_env_source=/opt/app_environment
 
 if [ ! -f ${source} ]; then
     touch ${source} && chmod 0644 ${source}
 fi
 
-if [ ! -f ${app_env_source} ]; then
-  touch ${app_env_source} & chmod 0644 ${app_env_source}
-fi
-
 echo "#!/bin/bash" > ${source}
+
+if [ ! -f /root/.bashrc ]; then
+    touch /root/.bashrc
+fi
 
 if ! grep -Fxq '$pattern' /root/.bashrc ; then
     echo -e "${pattern}" >> /root/.bashrc
-fi
-
-echo ${environment} > ${app_env_source}
-
-#----------------------------------------------------------
-# Add them as fastcgi_params
-#----------------------------------------------------------
-param_file=/etc/nginx/conf.d/params
-
-if [ ! -f ${param_file} ]; then
-  touch ${param_file} && chmod 0644 ${param_file}
 fi
 
 #----------------------------------------------------------
 # Generate the php-fpm config file
 #----------------------------------------------------------
 
-conf=/etc/php7/php-fpm.d/www.conf
+conf=/etc/php7/php-fpm.d/envars.conf
 
 if [ ! -f ${conf} ]; then
     touch ${conf} && chmod 0644 ${conf}
 fi
 
 # Clear and set the file for environment variables
-#echo "[www]" > ${conf}
+echo "[www]" > ${conf}
 
 #----------------------------------------------------------
 # Generate the php-cli ini file
@@ -98,18 +82,18 @@ echo "<?php return [" > ${lfourenv}
 
 echo "Generating Environment Variables"
 
-for var in ${vars}
-do
-    var=${var//[$'\'\t\r\n ']}
-    name=$(echo $var | cut -f1 -d=)
-    value=$(echo $var | cut -f2 -d=)
+cd /var/run/s6/container_environment
 
+for f in *; do
+  if [ -n "$( cat $f )" ]; then
+    name=${f}
+    value=`cat ${f}`
     echo "export ${name}='${value}'" >> ${source}
     echo "env[${name}] = '${value}'" >> ${conf}
     echo "env[${name}] = '${value}'" >> ${ini}
     echo "'${name}' => '${value}'," >> ${lfourenv}
     echo "${name}='${value}'" >> ${lfiveenv}
-    echo "fastcgi_param ${name} ${value};" >> ${param_file}
+  fi
 done
 
 #----------------------------------------------------------
